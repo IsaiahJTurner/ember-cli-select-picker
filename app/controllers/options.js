223edec206bff80bc9cdf718c75914c193aeb20d
Devin Weaver <suki@tritarget.org>
2015-05-02 00:46:56 -0400

    Add options page
    Only has showBadge for now

---
+++ app/controllers/options.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+function neighborhood() {
+  return chance.pick(['East side', 'West side']);
+}
+
+export default Ember.Controller.extend({
+  prepMultipleValue: function() {
+    console.log('hjfvhdfbej');
+    var sample = this.get('multipleContent').slice(0, 4);
+    this.set('multipleValue', sample);
+  }.on('init'),
+
+  singleContent: function() {
+    return chance.unique(chance.street, 10)
+      .map(function(street) {
+        return {label: street, value: street};
+      });
+  }.property(),
+
+  multipleContent: function() {
+    return chance.unique(chance.street, 10)
+      .map(function(street) {
+        return {label: street, value: street, group: neighborhood()};
+      })
+      .sortBy('group');
+  }.property(),
+
+  listContent: function() {
+    return chance.unique(chance.street, 10)
+      .map(function(street) {
+        return {label: street, value: street, group: neighborhood()};
+      })
+      .sortBy('group');
+  }.property()
+});
